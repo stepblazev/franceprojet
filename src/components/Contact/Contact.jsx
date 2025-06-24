06d2c4ec7a69a568ea980324e5dd489591beedf3
stepblazev <ohvatuki@gmail.com>
2025-06-24 20:08:12 +0300

    Contact / formatting file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,13 +7,14 @@ import { Controller, useForm } from 'react-hook-form';
 import Local from '../../assets/contact_address.svg';
 import Mail from '../../assets/contact_mail.svg';
 import Phone from '../../assets/contact_phone.svg';
-import axios  from 'axios';
+import axios from 'axios';
 import SuccessSubmit from '../UI/Modal/SuccessSubmit';
+
 const Contact = () => {
     const { t } = useTranslation('common');
- // Внутри функционального компонента
+    // Внутри функционального компонента
     const router = useRouter();
-    const { isOpen, onClose, onOpen } = useDisclosure()
+    const { isOpen, onClose, onOpen } = useDisclosure();
 
     const {
         control,
@@ -26,43 +27,151 @@ const Contact = () => {
             company: '',
             email: '',
             phone: '',
-        }
+        },
     });
 
-    const onSubmit = async (data) => {
-
-
-        await axios.post(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/forms/contact?yourName=${data.name}&yourSociety=${data.company}&yourEmail=${data.email}&yourPhone=${data.phone}`).then((response) => {
-            if(response.data.status == 1){
-                onOpen();
-            }            
-        })
-            .catch((error) => {
+    const onSubmit = async data => {
+        await axios
+            .post(
+                `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/forms/contact?yourName=${data.name}&yourSociety=${data.company}&yourEmail=${data.email}&yourPhone=${data.phone}`,
+            )
+            .then(response => {
+                if (response.data.status == 1) {
+                    onOpen();
+                }
+            })
+            .catch(error => {
                 console.log(error);
             });
 
         reset();
-    }
-
+    };
 
     return (
-        <SectionLayuot props={{  px: { base: '0px!important' }, maxH: '869px', h: 'auto' , id: 'contact',  overflow: 'hidden', mt: router.pathname === '/' ? { base: '50px', md: "100px" ,lg:'180px' } : { base: '50px', md: "100px" ,lg:'135px' }}}>
-            <Flex w={'100%'} h={'auto'} justifyContent={{base:'center', xlg: 'unset'}}>
-                <Flex w={{base: '100%', xxxsm:'fit-content'}} h={'auto'} justifyContent={{base: 'center',xsm:'flex-start'}} alignItems={'center'} p={{base:'80px 20px 55px',xxxsm:'40px',xlg:'77px 65px'}} flexDir={'column'} bgColor={{base:'#eeeeee',xxxsm:'#2E3083'}} gap={{base:'20px',xlg:'40px',smdesk:'70px'}}>
-                    <Text as={'h2'} fontSize={{ base: '32px',xxxsm:'44px', xlg: '64px' }}  fontWeight={{ base: '700', md: '800' }} color={{base:'#333333',xxxsm:'#ffffff'}} textAlign={'center'} lineHeight={{ base: 'normal', xlg: '76px' }} textTransform={{ base: 'uppercase',xxxsm:'capitalize' }}>{t('contact')}</Text>
-                    {/* <Text as={'h2'} fontSize={{ base: '32px', md: '32px' }} display={{ base: 'block', md: 'none' }} fontWeight={{ base: '700', md: '800' }} color={'#333333'} textAlign={'center'} lineHeight={{ base: '43px', md: '43px' }} textTransform={{ base: 'uppercase' }}>{t('contact}</Text> */}
-                    <Flex flexDir={'column'} gap={{ base: '12px', lg: '20px' }} display={{ base: 'none', xmd: 'flex' }}>
-                        <Flex flexWrap={'nowrap'} gap={{ base: '20px', lg: '40px' }} height={'22px'} alignItems={'start'}><Box pl={'5px'} w={'25px'}><Phone /></Box> <Text as={'p'} color={'#ffffff'} whiteSpace={'nowrap'} fontSize={{ base: '12px', lg: '18px' }} fontWeight={600} lineHeight={'normal'} textAlign={'left'}>01 02 03 04</Text> </Flex>
-                        <Flex flexWrap={'nowrap'} gap={{ base: '20px', lg: '40px' }} height={'22px'} alignItems={'start'}><Box w={'25px'}><Mail /> </Box><Text as={'p'} color={'#ffffff'} whiteSpace={'nowrap'} fontSize={{ base: '12px', lg: '18px' }} fontWeight={600} lineHeight={'normal'} textAlign={'left'}>05contact@franceprojet.com1</Text> </Flex>
-                        <Flex flexWrap={'nowrap'} gap={{ base: '20px', lg: '40px' }} height={'22px'} alignItems={'start'}><Box pl={'5px'} w={'25px'}><Local /></Box> <Text as={'p'} whiteSpace={'nowrap'} color={'#ffffff'} fontSize={{ base: '12px', lg: '18px' }} fontWeight={600} lineHeight={'normal'} textAlign={'left'}>rue de paris 75000 Paris</Text> </Flex>
+        <SectionLayuot
+            props={{
+                px: { base: '0px!important' },
+                maxH: '869px',
+                h: 'auto',
+                id: 'contact',
+                overflow: 'hidden',
+                mt:
+                    router.pathname === '/'
+                        ? { base: '50px', md: '100px', lg: '180px' }
+                        : { base: '50px', md: '100px', lg: '135px' },
+            }}
+        >
+            <Flex w={'100%'} h={'auto'} justifyContent={{ base: 'center', xlg: 'unset' }}>
+                <Flex
+                    w={{ base: '100%', xxxsm: 'fit-content' }}
+                    h={'auto'}
+                    justifyContent={{ base: 'center', xsm: 'flex-start' }}
+                    alignItems={'center'}
+                    p={{ base: '80px 20px 55px', xxxsm: '40px', xlg: '77px 65px' }}
+                    flexDir={'column'}
+                    bgColor={{ base: '#eeeeee', xxxsm: '#2E3083' }}
+                    gap={{ base: '20px', xlg: '40px', smdesk: '70px' }}
+                >
+                    <Text
+                        as={'h2'}
+                        fontSize={{ base: '32px', xxxsm: '44px', xlg: '64px' }}
+                        fontWeight={{ base: '700', md: '800' }}
+                        color={{ base: '#333333', xxxsm: '#ffffff' }}
+                        textAlign={'center'}
+                        lineHeight={{ base: 'normal', xlg: '76px' }}
+                        textTransform={{ base: 'uppercase', xxxsm: 'capitalize' }}
+                    >
+                        {t('contact')}
+                    </Text>
+                    <Flex
+                        flexDir={'column'}
+                        gap={{ base: '12px', lg: '20px' }}
+                        display={{ base: 'none', xmd: 'flex' }}
+                    >
+                        <Flex
+                            flexWrap={'nowrap'}
+                            gap={{ base: '20px', lg: '40px' }}
+                            height={'22px'}
+                            alignItems={'start'}
+                        >
+                            <Box pl={'5px'} w={'25px'}>
+                                <Phone />
+                            </Box>{' '}
+                            <Text
+                                as={'p'}
+                                color={'#ffffff'}
+                                whiteSpace={'nowrap'}
+                                fontSize={{ base: '12px', lg: '18px' }}
+                                fontWeight={600}
+                                lineHeight={'normal'}
+                                textAlign={'left'}
+                            >
+                                01 02 03 04
+                            </Text>{' '}
+                        </Flex>
+                        <Flex
+                            flexWrap={'nowrap'}
+                            gap={{ base: '20px', lg: '40px' }}
+                            height={'22px'}
+                            alignItems={'start'}
+                        >
+                            <Box w={'25px'}>
+                                <Mail />{' '}
+                            </Box>
+                            <Text
+                                as={'p'}
+                                color={'#ffffff'}
+                                whiteSpace={'nowrap'}
+                                fontSize={{ base: '12px', lg: '18px' }}
+                                fontWeight={600}
+                                lineHeight={'normal'}
+                                textAlign={'left'}
+                            >
+                                05contact@franceprojet.com1
+                            </Text>{' '}
+                        </Flex>
+                        <Flex
+                            flexWrap={'nowrap'}
+                            gap={{ base: '20px', lg: '40px' }}
+                            height={'22px'}
+                            alignItems={'start'}
+                        >
+                            <Box pl={'5px'} w={'25px'}>
+                                <Local />
+                            </Box>{' '}
+                            <Text
+                                as={'p'}
+                                whiteSpace={'nowrap'}
+                                color={'#ffffff'}
+                                fontSize={{ base: '12px', lg: '18px' }}
+                                fontWeight={600}
+                                lineHeight={'normal'}
+                                textAlign={'left'}
+                            >
+                                rue de paris 75000 Paris
+                            </Text>{' '}
+                        </Flex>
                     </Flex>
-                    <Flex as='form'  flexDir={'column'} alignItems={'end'} gap={'16px'} w={{ base:  '100%',sm:'80%',xxxsm: '100%', lg: '350px', xlg: '346px' }} onSubmit={handleSubmit(onSubmit)}>
+                    <Flex
+                        as="form"
+                        flexDir={'column'}
+                        alignItems={'end'}
+                        gap={'16px'}
+                        w={{ base: '100%', sm: '80%', xxxsm: '100%', lg: '350px', xlg: '346px' }}
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
                         <Controller
                             name="name"
                             id="name"
                             control={control}
                             render={({ field }) => (
-                                <Input {...field}   placeholder={t('yourName')} w={'100%'} borderRadius={'2px'} bgColor={'white'} />
+                                <Input
+                                    {...field}
+                                    placeholder={t('yourName')}
+                                    w={'100%'}
+                                    borderRadius={'2px'}
+                                    bgColor={'white'}
+                                />
                             )}
                         />
                         <Controller
@@ -70,7 +179,13 @@ const Contact = () => {
                             id="company"
                             control={control}
                             render={({ field }) => (
-                                <Input {...field}   placeholder={t('yourCompany')} w={'100%'} borderRadius={'2px'} bgColor={'white'} />
+                                <Input
+                                    {...field}
+                                    placeholder={t('yourCompany')}
+                                    w={'100%'}
+                                    borderRadius={'2px'}
+                                    bgColor={'white'}
+                                />
                             )}
                         />
                         <Controller
@@ -78,7 +193,13 @@ const Contact = () => {
                             id="email"
                             control={control}
                             render={({ field }) => (
-                                <Input {...field} placeholder={t('yourEmail')} w={'100%'} borderRadius={'2px'} bgColor={'white'} />
+                                <Input
+                                    {...field}
+                                    placeholder={t('yourEmail')}
+                                    w={'100%'}
+                                    borderRadius={'2px'}
+                                    bgColor={'white'}
+                                />
                             )}
                         />
 
@@ -87,17 +208,17 @@ const Contact = () => {
                             id="phone"
                             control={control}
                             render={({ field }) => (
-                                <Input {...field} placeholder={t('yourPhone')} w={'100%'} borderRadius={'2px'} bgColor={'white'} />
+                                <Input
+                                    {...field}
+                                    placeholder={t('yourPhone')}
+                                    w={'100%'}
+                                    borderRadius={'2px'}
+                                    bgColor={'white'}
+                                />
                             )}
                         />
-                        <button
-
-                            className='form_btn'
-                            
-                            
-                            type='submit'
-                        >{t('sendRequest')}
-
+                        <button className="form_btn" type="submit">
+                            {t('sendRequest')}
                         </button>
                     </Flex>
                 </Flex>
@@ -111,7 +232,7 @@ const Contact = () => {
                     ></iframe>
                 </Box>
             </Flex>
-            <SuccessSubmit  isOpen={isOpen} onClose={onClose}/>
+            <SuccessSubmit isOpen={isOpen} onClose={onClose} />
         </SectionLayuot>
     );
 };
