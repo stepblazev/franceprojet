808b7ddfdb2f3ae672e30652ac92db2de7cd43c2
stepblazev <ohvatuki@gmail.com>
2025-06-25 22:01:46 +0300

    Project page feedback

--- src/pages/project/[id].js
+++ src/pages/project/[id].js
@@ -1,13 +1,16 @@
 import Contact from '@/components/Contact/Contact';
 import Projects from '@/components/Projects/Projects';
 import SectionLayuot from '@/components/UI/Layouts/SectionLayuot';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Box } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { getOneProject } from '../api/api';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
+import { ImQuotesLeft } from "react-icons/im";
+import theme from '@/styles/theme';
+
 
 const ProjectItem = ({ project }) => {
     // console.log("🚀 ~ ProjectItem ~ project:", project)
@@ -68,29 +71,56 @@ const ProjectItem = ({ project }) => {
                     color={'#2E3083'}
                     dangerouslySetInnerHTML={{ __html: project?.text }}
                 ></Text>
-                {project?.youtube && (
-                    <Flex
-                        borderRadius={'0px'}
-                        aspectRatio={{ base: '16/9', desk: '16/5' }}
-                        w="100%"
-                        //  maxW={'1709px'}
-                        overflow={'hidden'}
-                        justify={'center'}
-                        align="center"
-                        border={'1px solid  #C7CCD3'}
-                        //    h={{ base: "389px" }}
-                        pos="relative"
-                        mb={'40px'}
-                    >
-                        <iframe
-                            width="100%"
-                            height="100%"
-                            src={project?.youtube}
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            //   allowFullScreen
-                        />
+            
+                <Flex
+                    borderRadius={'0px'}
+                    w="100%"
+                    direction={{ base: 'column', xlg: 'row' }}
+                    overflow={'hidden'}
+                    justify={'center'}
+                    align={'center'}
+                    gap={'64px'}
+                    mb={'40px'}
+                >
+                    {project?.youtube && (
+                        <Box flexBasis={{ base: 'auto', xlg: '50%'}} alignSelf={{ base: 'stretch', xlg: 'center' }}>
+                            <Box paddingBottom={'56.25%'} position={'relative'}>
+                                <iframe
+                                    style={{ position: 'absolute', top: 0, left: 0 }}
+                                    width="100%"
+                                    height="100%"
+                                    src={project?.youtube}
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    //   allowFullScreen
+                                />
+                            </Box>
+                        </Box>
+                    )}
+                    <Flex flexBasis={{ base: 'auto', xlg:'50%'}} justifyContent={'center'} alignItems={'center'}>
+                        <Box w={{ base: '100%', xmd: '75%'}} backgroundColor={'#F5F5F5'} padding={{ base: '20px', sm: '32px 32px 18px 32px'}}>
+                            <Flex justify={'space-between'} align={'center'} color={theme.colors.d_black}>
+                                <ImQuotesLeft fontSize={'40px'} />
+                                <Text as={'p'} fontSize={'24px'} fontWeight={700} textTransform={'uppercase'} whiteSpace={'nowrap'}>
+                                    Feedback
+                                </Text>
+                            </Flex>
+                            <Text mt={'24px'} as={'p'} fontSize={{ base: '12px', xmini: '16px' }} lineHeight={'1.75'} fontStyle={'italic'}>
+                                I recently used a construction service for a home renovation project, and I couldn't be happier with the results. The team was professional, efficient, and knowledgeable throughout the entire process. They communicated with me regularly to ensure that everything was going according to plan and made sure that any issues were promptly addressed. 
+                            </Text>
+                            <Box mt={'24px'}>
+                                <Flex alignItems={'stretch'} gap={'16px'}>
+                                    <Box w={'50px'} h={'50px'} position={'relative'} rounded={'50%'} overflow={'hidden'}>
+                                        <Box w={'100%'} h={'100%'} objectFit={'cover'} objectPosition={'center center'} backgroundColor={theme.colors.d_black}></Box>
+                                    </Box>
+                                    <Flex direction={'column'} gap={'8px'} lineHeight={'1'}>
+                                        <Text as={'p'} fontSize={'18px'} fontWeight={600}>John Doe</Text>
+                                        <Text as={'p'} fontSize={'12px'} fontWeight={400} opacity={0.5}>Subtitle</Text>
+                                    </Flex>
+                                </Flex>
+                            </Box>
+                        </Box>
                     </Flex>
-                )}
+                </Flex>
 
                 <Flex
                     flexDir={{ base: 'column', xmd: 'row' }}
