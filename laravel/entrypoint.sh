362e104a748b4284bc1914d86fef933fc2fb05eb
stepblazev <ohvatuki@gmail.com>
2025-11-10 18:35:30 +0300

    laravel fix

--- laravel/entrypoint.sh
+++ laravel/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 php artisan storage:link
 php artisan migrate --force
-apache2-foreground
+
+exec apache2-foreground
